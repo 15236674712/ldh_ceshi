3758b579c600ce552cb35889187022e766b35b30
zhangcn <15236674712@163.com>
2020-12-01 11:31:36 +0800

    小黑:实现类别模块功能

---
+++ src/main/java/com/baizhi/dao/CategoryDao.java
@@ -0,0 +1,10 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Category;
+
+public interface CategoryDao {
+
+
+    Category qyertById(String id);
+
+}
---
+++ src/main/java/com/baizhi/entity/Category.java
@@ -0,0 +1,9 @@
+package com.baizhi.entity;
+
+public class Category {
+
+
+    private String id;
+    private String name;
+
+}
