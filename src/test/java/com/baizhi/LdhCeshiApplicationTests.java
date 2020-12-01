b210b5f5caf91007c08c92fe2e4bc6849083b69c
zhangcn <15236674712@163.com>
2020-12-01 10:19:00 +0800

    测试类添加变量

--- src/test/java/com/baizhi/LdhCeshiApplicationTests.java
+++ src/test/java/com/baizhi/LdhCeshiApplicationTests.java
@@ -9,6 +9,11 @@ class LdhCeshiApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("====");
+
+        int a = 2147483646;
+        a = a + 3;
+        System.out.println(a);
+        //-2147483647
     }
 
 }
