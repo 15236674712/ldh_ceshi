dbc029c087f014dcde737ba4b3a1d0c2c1e5d281
zhangcn <15236674712@163.com>
2020-12-01 10:20:04 +0800

    添加一个测试类

---
+++ src/test/java/com/baizhi/GitTests.java
@@ -0,0 +1,19 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("====");
+
+        int a = 2147483646;
+        a = a + 3;
+        System.out.println(a);
+        //-2147483647
+    }
+
+}
