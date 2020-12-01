b0cdf9a628c3c3c4a24404c5beacb0d6431d5218
zhangcn <15236674712@163.com>
2020-12-01 11:28:24 +0800

    小黄:视频模块开发2

---
+++ src/main/java/com/baizhi/dao/VideoDao.java
@@ -0,0 +1,11 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Video;
+
+import java.util.List;
+
+public interface VideoDao {
+
+    List<Video> queryByVideoName(String title);
+
+}
---
+++ src/main/java/com/baizhi/entity/Video.java
@@ -0,0 +1,10 @@
+package com.baizhi.entity;
+
+public class Video {
+
+    public String id;
+    public String title;
+    public String cover;
+    public String path;
+
+}
